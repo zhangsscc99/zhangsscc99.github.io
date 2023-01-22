205c5cfec64b9418942c9d1c521b0b21e8d63ea5
zhangsscc99 <yw4554@nyu.edu>
2023-01-21 21:30:18 -0500

    update

--- 912sortanarray.py
+++ 912sortanarray.py
@@ -11,5 +11,15 @@ class Solution():
                         nums[j]=nums[j+1]
                         nums[j+1]=temp
             return nums
+    def sortArray(self,nums):
+        print('haha')
+        for i in range(1,len(nums)):
+            value=nums[i]
+            j=i-1
+            while j>=0 and nums[j]>value:
+                nums[j+1]=nums[j]
+                j-=1
+            nums[j+1]=value
+        return nums
 s=Solution()
 print(s.sortArray([2,1,3,5,4]))

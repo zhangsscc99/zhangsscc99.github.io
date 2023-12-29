0e6b1b563740658651febc4da3708a5b8df7012d
Yuting Wang <yw4554@nyu.edu>
2023-12-29 05:44:37 -0600

    104

---
+++ 104maxDepth.java
@@ -0,0 +1,27 @@
+class Solution {
+    int res = 0;
+    int depth = 0;
+    public int maxDepth(TreeNode root) {
+        traverse(root);
+        return res;
+
+
+        
+
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        depth++;
+        if (root.left == null && root.right == null) {
+            res = Math.max(res, depth);
+        }
+        traverse(root.left);
+        traverse(root.right);
+        depth--;
+        
+    }
+
+}

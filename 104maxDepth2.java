9368a64d7df352b9c3ca17aaccf16114bb5f556d
Yuting Wang <yw4554@nyu.edu>
2023-12-29 05:47:18 -0600

    104 recursion

---
+++ 104maxDepth2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        return 1 + Math.max(leftMax, rightMax);
+
+    }
+}

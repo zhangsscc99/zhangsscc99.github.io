02792f81cedb3dfe4aa4e38fb988224a229c15f5
zhangsscc99 <yw4554@nyu.edu>
2023-12-30 00:35:47 -0600

    106constructBinaryTree

---
+++ 106constructBinaryTree.java
@@ -0,0 +1,26 @@
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+
+    }
+
+    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int rootVal = postorder[postEnd];
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(inorder, inStart, index - 1, postorder,postStart, postStart + leftSize - 1);
+        root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+}

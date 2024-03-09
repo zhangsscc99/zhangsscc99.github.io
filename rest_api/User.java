b6d710db5f18ba58783bb2b12c448b9b8f0155e4
Yuting Wang <yw4554@nyu.edu>
2024-03-09 11:30:43 -0600

    rest api

---
+++ rest_api/User.java
@@ -0,0 +1,13 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class User{
+    private Long id;
+    private String username;
+    private String email;
+
+    public String 
+
+}

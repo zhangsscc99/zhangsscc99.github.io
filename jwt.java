2794c08692116731051b0b554a67f88f42e31bae
Yuting Wang <yw4554@nyu.edu>
2024-06-19 14:05:43 +0800

    jest

--- jwt.java
+++ jwt.java
@@ -3,6 +3,14 @@ import com.alibaba.fastjson.JSONObject;
 public class JWTDemo05 {
     public static void main(String[] args) {
         JSONObject header = new JSONObject();
-        header.putu("Alg", "HS256");
+        header.put("Alg", "HS256");
+        JSONObject payLoad = new JSONObject();
+        payLoad.put("userName", "mayikt644");
+        payLoad.put("userAge", "21");
+        payLoad.put("UserAge", "21");
+        payLoad.put("UserId", "18888");
+        String jwtHeader = Base64.getEncoder().encodeToString(header.toJSONString().getBytes());
+        String jwtPayLoad = Base64.getEncoder().encodeToString(payLoad.toJSONString().getBytes());
+        //md5防止接口参数被篡改
     }
 }

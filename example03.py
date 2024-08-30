e4bcbee6fb97fa15e8ceadedc4098ca7759553af
Yuting Wang <yw4554@nyu.edu>
2024-08-30 18:57:37 -0500

    tcp ip

--- example03.py
+++ example03.py
@@ -1,2 +1,10 @@
-stk1 = []
-print(stk1[-1])
+
+def findLengthOfLCIS(nums):
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                dp[i] = dp[i - 1] + 1
+        return max(dp)
+
+nums = [2,1,200,100,99]
+print(findLengthOfLCIS(nums))
